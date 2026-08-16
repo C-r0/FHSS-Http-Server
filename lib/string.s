e5f76d67bd45b4cab59363ea514de6d3fe3be472
C-r0 <146779565+C-r0@users.noreply.github.com>
2026-08-16 15:01:38 +0000

    Added inttostr

--- lib/string.s
+++ lib/string.s
@@ -1,9 +1,13 @@
 .intel_syntax noprefix
 
-.text
+.section .text
 .globl print
 .type print, @function
 print:
+    # rdi = fd
+    # rsi = buf
+    # rdx = len
+
     mov rax, 1
     syscall
     ret
@@ -11,8 +15,8 @@ print:
 .globl strcmp
 .type strcmp, @function
 strcmp:
-    # rdi
-    # rsi
+    # rdi = str1
+    # rsi = str2
 .strcmp_loop:
     mov al, byte [rdi]
     cmp al, byte[rsi]
@@ -32,7 +36,8 @@ strcmp:
 .globl strlen
 .type strlen, @function
 strlen:
-    # rdi
+    # rdi = str
+
     xor rax, rax
 .strlen_loop:
     cmp byte ptr [rdi + rax], 0
@@ -40,4 +45,47 @@ strlen:
     inc rax
     jmp .strlen_loop
 .strlen_done:
+    ret
+
+.globl inttostr
+.type inttostr, @function
+inttostr:
+    # RECEIVE
+    # rdi = int
+    # rsi = buf
+    # SEND
+    # rax = int
+    # rdx = int
+
+    mov r10, rsi
+
+    test rdi, rdi
+    je .inttostr_zero
+
+    mov rax, rdi
+    xor rcx, rcx
+    mov rbx, 10
+.inttostr_loop:
+    xor edx, edx
+    div rbx
+    add dl, 48
+    push rdx
+    inc rcx
+    test rax, rax
+    jne .inttostr_loop
+.inttostr_save_nofchar:
+    mov r8, rcx
+.inttostr_write_string:
+    pop rdx
+    mov byte ptr [r10], dl
+    inc r10
+    dec rcx
+    jne .inttostr_write_string
+    mov byte ptr [r10], 0
+    mov rax, r8
+    ret
+.inttostr_zero:
+    mov byte ptr [r10], '0'
+    mov byte ptr [r10+1], 0
+    xor rax, rax
     ret
