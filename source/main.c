e5028f19e1b6da0c115cb8afbfc6405400fb0948
C-r0 <146779565+C-r0@users.noreply.github.com>
2026-08-16 15:00:51 +0000

    Added way to print the port

--- source/main.c
+++ source/main.c
@@ -1,11 +1,15 @@
 extern int print(int fd, const void *buf, int len);
 extern int strcmp(const void *str1, const void *str2);
 extern int strlen(const void *str);
+extern int inttostr(int num, char *buf);
 
 __attribute__((visibility("default")))
 int start_server(int port) {
-    const char startMsg[] = "Serving on Port: ";
-    print(1, startMsg, sizeof(startMsg));
+    print(1, "Serving on Port: ", 17);
+    char buf[32];
+    int len = inttostr(port, buf);
+    print(1, buf, len);
+    print(1, "\n", 1);
     
     return 0;
 }
