b601f2511acd07caaba1b25f3d3890539ad76f98
C-r0 <146779565+C-r0@users.noreply.github.com>
2026-08-16 15:00:19 +0000

    Added way for compile example C

--- compile.sh
+++ compile.sh
@@ -2,9 +2,13 @@
 
 mkdir -p build
 
-gcc -fPIC -c lib/string.s -o build/string_so.o
+gcc -fPIC -c lib/string.s -o build/string.o
 gcc -fPIC -c lib/start.s -o build/start.o
 
 gcc -fPIC -c -fno-builtin -fno-stack-protector source/main.c -o build/main.o
 
-gcc -shared -nostdlib build/string_so.o build/start.o build/main.o -o build/libfhss.so
+gcc -shared -nostdlib build/string.o build/start.o build/main.o -o build/libfhss.so
+
+# EXAMPLE C
+# gcc -I./include -L./build -o build/exampleC examples/C/example.c -lfhss
+# LD_LIBRARY_PATH=./build ./build/exampleC
